c46c0ade5fe22209baadb95cb5186a20950cc44f
Theoszt <gerardnainggolan906@gmail.com>
2025-08-14 16:01:47 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GMAIL_USER = os.getenv("GMAIL_USER")
-GMAIL_APP_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
-RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
