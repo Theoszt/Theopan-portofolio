d1e8e37b46bbd12aedb936663e870a0a5ab5fa33
Theoszt <gerardnainggolan906@gmail.com>
2025-08-15 10:14:44 +0700

    Update main.py

--- back_end/main.py
+++ back_end/main.py
@@ -6,11 +6,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://Theoszt.github.io",
-                  "https://theopan-portofolio27.vercel.app"],  
+    allow_origins=["https://theopan-portofolio27.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+@app.get("/")
+def read_root():
+    return {"message": "Backend is running"}
 
 app.include_router(router, prefix="/api")
